9b6853fcf31b10a1b04c503d2a54f4058fa9698c
Veerapallirajesh <your_email@example.com>
2025-12-25 21:51:34 +0000

    Add shared VPC outputs

--- environments/shared/outputs.tf
+++ environments/shared/outputs.tf
@@ -5,5 +5,16 @@ output "admin_role_name" {
 output "readonly_role_name" {
   value = module.iam.readonly_role_name
 }
+output "shared_vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "shared_public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "shared_private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
 
 
